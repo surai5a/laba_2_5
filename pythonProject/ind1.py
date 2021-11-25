1ce95ea0aad80abf601d1d7789bf2088418d0df5
Vadim <vadym.prymary@gmail.com>
2021-11-25 14:41:44 +0300

    Squashed commit of the following:
    commit e94536bd8d3bd52a5962d4c556291fb20bdb3357
    Author: Vadim <vadym.prymary@gmail.com>
    Date:   Thu Nov 25 14:41:30 2021 +0300
    
        Create Отчет 8 25.pdf
    
    commit 732bac3a924d0ad852d2fb26abd2b08a7a98bde8
    Author: Vadim <vadym.prymary@gmail.com>
    Date:   Thu Nov 25 14:40:07 2021 +0300
    
        fix
    
    commit e445c7907acb6d31395190e3963f0f5fc932e4e8
    Author: Vadim <vadym.prymary@gmail.com>
    Date:   Tue Nov 16 19:54:59 2021 +0300
    
        Update ind1.py
    
    commit c97922f3b317768665f1dd2e1e2fd6b042db5472
    Author: Vadim <vadym.prymary@gmail.com>
    Date:   Tue Nov 16 13:35:20 2021 +0300
    
        ex1 created

---
+++ pythonProject/ind1.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+import os
+# можно реализовать ввод данных с клавиатуры, но по заданию не было такого требования
+amnt = (12, 15, 0,   3,  2,  4, 10,  0, 0, 0) # количество осадков
+temp = (1,  -1, -2, -4, -5, -7, -5, -3, 0, 1) # температура
+wthr = ('Snow', 'Rain', 'No precipitation') # кортеж имен погоды
+
+
+def prec(a): #находит погоду по дню
+    if amnt[a] > 0 and temp[a] > 0:
+        wth = f"Weather is: {wthr[1]}\n" \
+              f"Temp: {temp[a]}\n" \
+              f"Amount: {amnt[a]}\n"
+
+        print(wth)
+    elif amnt[a] > 0 and temp[a] < 0:
+        wth = f"Weather is: {wthr[0]}\n" \
+              f"Temp: {temp[a]}\n" \
+              f"Amount: {amnt[a]}\n"
+        print(wth)
+    elif amnt[a] <= 0:
+        wth = f"Weather is: {wthr[2]}\n" \
+              f"Temp: {temp[a]}\n" \
+              f"Amount: {amnt[a]}\n"
+        print(wth)
+
+
+def amount(a, b): # находит кол - во дней с опр. погодой
+    amnt1 = amnt[a:(b + 1)]
+    temp1 = temp[a:(b + 1)]
+    rain = 0
+    snow = 0
+    nprc = 0;
+    for x, z in zip(amnt1, temp1):
+        if x > 0 and z > 0:
+            rain += 1
+        elif x > 0 and z < 0:
+            snow += 1
+        elif x == 0:
+            nprc += 1
+    print(f"Rainy days: {rain}\n"
+          f"Snowy days: {snow}\n"
+          f"No falls: {nprc}\n")
+
+
+def menu(): # меню для выбора функций
+    os.system('cls')
+    print(f"In database i have {len(amnt)} entries about weather\n"
+          f"What do you want to see:\n"
+          f"1 - Weather in a day\n"
+          f"2 - Precipitations for the period\n"
+          f"3 - Exit\n")
+
+    x = int(input())
+
+    if x == 1:
+        os.system('cls')
+        print(f"Which day (0 - {len(amnt) - 1}): ")
+        z = int(input())
+        if z > 9 or z < 0:
+            print("Wrong number!")
+            input("Press Enter to continue...")
+            menu()
+        else:
+            prec(z)
+            input("Press Enter to continue...")
+            menu()
+    elif x == 2:
+        os.system('cls')
+        print(f"What period (form - 'a b'): ")
+        tpl = tuple(map(int, input().split()))
+        a, b = tpl
+        if a > b or a > 9 or a < 0 or b > 9 or b < 0 or (a == 0 and b == 0):
+            print("Wrong number!")
+            input("Press Enter to continue...")
+            menu()
+        else:
+            amount(a, b)
+            input("Press Enter to continue...")
+            menu()
+    elif x == 3:
+        exit(0)
+
+
+if __name__ == '__main__':
+    menu()
